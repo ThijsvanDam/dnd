b5d49bb0e5b7906b79d5ea63727eb0108b710e3d
Thijs.vanDam <thijs.vandam@nedap.com>
2023-11-16 23:22:55 +0100

    initialize all base stuff - works like a charm

--- initialize.py
+++ initialize.py
@@ -1,26 +1,40 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+# from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
+
+from models.Base import Base
 from models.Player import Player
+from models.Character import Character
+
+# Let's read the following: https://medium.com/@sandyjtech/creating-a-database-using-python-and-sqlalchemy-422b7ba39d7e
+
+
 
 engine = create_engine("sqlite:///test.db", echo=True)
 
-Player.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 
-with Session(engine) as session:
-    players = [
-        Player(
-            name="Aehyam",
-            password="test123",
-            role="Admin",
-        ),
-        # Player(
-        #     name="Lex",
-        #     password="vlimvlom",
-        #     role="Nerd"
-        # ),
-    ]
+Session = sessionmaker(bind=engine)
 
-    session.add_all(players)
+session = Session()
 
-    session.commit()
+# with Session(engine) as session:
+
+players = [
+    Player(
+        name="Thijs",
+        password="test123",
+        role="Admin",
+    ),
+    Player(
+        name="Alex",
+        password="vlimvlom",
+        role="Nerd"
+    ),
+]
+
+session.add_all(players)
+
+
+session.commit()
